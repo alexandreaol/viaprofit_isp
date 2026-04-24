033b2dc138557b7c1b4ea2f5c29398be9a8bc9dc
alexandreaol <alexandre.ol.eng@gmail.com>
2026-04-24 16:26:25 -0300

    configuracao e teste de banco

--- config.php
+++ config.php
@@ -1,38 +1,46 @@
 <?php
 
-const SENHA_SISTEMA = '';
-const SENHA_VIAPROFIT = '';
+const DB_HOST = 'localhost';
+
+const DB_SISTEMA = 'u308598921_via_ccm';
+const DB_USER_SISTEMA = 'u308598921_via_ccm';
+const DB_PASS_SISTEMA = 'Mesmox400#';
+
+const DB_VIAPROFIT = 'u308598921_via_profit';
+const DB_USER_VIAPROFIT = 'u308598921_via_profit';
+const DB_PASS_VIAPROFIT = 'Mesmox400#';
 
 function conectarSistema()
 {
     return criarConexao(
-        'localhost',
-        'u308598921_via_ccm',
-        'u308598921_via_ccm',
-         'Mesmox400#'
+        DB_HOST,
+        DB_SISTEMA,
+        DB_USER_SISTEMA,
+        DB_PASS_SISTEMA
     );
 }
 
 function conectarViaprofit()
 {
     return criarConexao(
-        'localhost',
-        'u308598921_via_profit',
-        'u308598921_via_profit',
-        'Mesmox400#'
+        DB_HOST,
+        DB_VIAPROFIT,
+        DB_USER_VIAPROFIT,
+        DB_PASS_VIAPROFIT
     );
 }
 
 function criarConexao($host, $banco, $usuario, $senha)
 {
     try {
-        $dsn = "mysql:host={$host};dbname={$banco};charset=utf8";
+        $dsn = "mysql:host={$host};dbname={$banco};charset=utf8mb4";
 
         return new PDO($dsn, $usuario, $senha, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
         ]);
+
     } catch (PDOException $erro) {
         jsonResponse(false, 'Erro ao conectar ao banco de dados.', [
             'erro' => $erro->getMessage(),
